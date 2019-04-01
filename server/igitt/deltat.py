d5f902dcd103d91e1fc26141b6240aaf2e4b331c
Marcel Waldvogel <marcel.waldvogel@uni-konstanz.de>
2019-04-01 07:36:36 +0200

    Code readability

--- server/igitt/deltat.py
+++ server/igitt/deltat.py
@@ -10,12 +10,15 @@ import re
 from datetime import timedelta
 
 
-regex = re.compile(r'^((?P<days>[\.\d]+?)d)?((?P<hours>[\.\d]+?)h)?((?P<minutes>[\.\d]+?)m)?((?P<seconds>[\.\d]+?)s)?$')
+regex = re.compile(r'^((?P<days>[\.\d]+?)d)?'
+                   r'((?P<hours>[\.\d]+?)h)?'
+                   r'((?P<minutes>[\.\d]+?)m)?'
+                   r'((?P<seconds>[\.\d]+?)s)?$')
 
 
 def parse_time(time_str):
     """
-    Parse a time string e.g. (2h13m) into a timedelta object.
+    Parse a time string e.g. "2h13m" into a timedelta object.
 
     Based on Peter's answer at https://stackoverflow.com/a/51916936/2445204
     and virhilo's answer at https://stackoverflow.com/a/4628148/851699
@@ -24,6 +27,8 @@ def parse_time(time_str):
     :return datetime.timedelta: A datetime.timedelta object
     """
     parts = regex.match(time_str)
-    assert parts is not None, "Could not parse any time information from '{}'.  Examples of valid strings: '8h', '2d8h5m20s', '2m4s'".format(time_str)
-    time_params = {name: float(param) for name, param in parts.groupdict().items() if param}
+    assert parts is not None, """Could not parse any time information from '{}'.
+    Examples of valid strings: '8h', '2d8h5m20s', '2m4s'""".format(time_str)
+    time_params = {name: float(param)
+            for name, param in parts.groupdict().items() if param}
     return timedelta(**time_params)
