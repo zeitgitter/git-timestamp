ea4fd107d2aef47136c5a43b20e607ce35a340d5
Marcel Waldvogel <marcel.waldvogel@uni-konstanz.de>
2019-04-01 07:25:59 +0200

    Adding timespan parser
    Taken verbatim from Peter at https://stackoverflow.com/a/51916936/2445204

---
+++ server/igitt/deltat.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+import re
+from datetime import timedelta
+
+
+regex = re.compile(r'^((?P<days>[\.\d]+?)d)?((?P<hours>[\.\d]+?)h)?((?P<minutes>[\.\d]+?)m)?((?P<seconds>[\.\d]+?)s)?$')
+
+
+def parse_time(time_str):
+    """
+    Parse a time string e.g. (2h13m) into a timedelta object.
+
+    Modified from virhilo's answer at https://stackoverflow.com/a/4628148/851699
+
+    :param time_str: A string identifying a duration.  (eg. 2h13m)
+    :return datetime.timedelta: A datetime.timedelta object
+    """
+    parts = regex.match(time_str)
+    assert parts is not None, "Could not parse any time information from '{}'.  Examples of valid strings: '8h', '2d8h5m20s', '2m4s'".format(time_str)
+    time_params = {name: float(param) for name, param in parts.groupdict().items() if param}
+    return timedelta(**time_params)
