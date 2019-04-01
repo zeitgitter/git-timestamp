e5a2545d766d3f11d01517a704e4ff36180f56c2
Marcel Waldvogel <marcel.waldvogel@uni-konstanz.de>
2019-04-01 07:40:58 +0200

    Changed for readability of time strings

--- server/igitt/deltat.py
+++ server/igitt/deltat.py
@@ -10,25 +10,25 @@ import re
 from datetime import timedelta
 
 
-regex = re.compile(r'^((?P<days>[\.\d]+?)d)?'
-                   r'((?P<hours>[\.\d]+?)h)?'
-                   r'((?P<minutes>[\.\d]+?)m)?'
+regex = re.compile(r'^((?P<days>[\.\d]+?)d)? *'
+                   r'((?P<hours>[\.\d]+?)h)? *'
+                   r'((?P<minutes>[\.\d]+?)m)? *'
                    r'((?P<seconds>[\.\d]+?)s)?$')
 
 
 def parse_time(time_str):
     """
-    Parse a time string e.g. "2h13m" into a timedelta object.
+    Parse a time string e.g. '2h 13m' or '1.5d' into a timedelta object.
 
     Based on Peter's answer at https://stackoverflow.com/a/51916936/2445204
     and virhilo's answer at https://stackoverflow.com/a/4628148/851699
 
-    :param time_str: A string identifying a duration.  (eg. 2h13m)
+    :param time_str: A string identifying a duration, e.g. '2h13.5m'
     :return datetime.timedelta: A datetime.timedelta object
     """
     parts = regex.match(time_str)
     assert parts is not None, """Could not parse any time information from '{}'.
-    Examples of valid strings: '8h', '2d8h5m20s', '2m4s'""".format(time_str)
+    Examples of valid strings: '8h', '2d 8h 5m 2s', '2m4.3s'""".format(time_str)
     time_params = {name: float(param)
             for name, param in parts.groupdict().items() if param}
     return timedelta(**time_params)
